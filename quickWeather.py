b26823ee46b5746426e8b04362463e3389421141
LincolnBrownBellevue <librown@my365.bellevue.edu>
2021-03-17 18:37:32 -0600

    Works with zip

--- quickWeather.py
+++ quickWeather.py
@@ -1,15 +1,26 @@
 #! python3
 # quickWeather.py - Prints the weather for a location from the command line.
 
-import json, requests, sys
+import json, requests, sys, pprint
 
 #Compute location from command line arguments.
-if len(sys.argv) < 2:
-    print('Usage: quickWeather.py location')
-    sys.exit()
-location = ' '.join*sys.argv[1:])
+zip = ' '.join(sys.argv[1:])
 
-#TODO: Download the JSON data from OpenWeatherMap.org's API.
+# Download the JSON data from OpenWeatherMap.org's API.
+url = 'https://api.openweathermap.org/data/2.5/weather?zip=%s&appid=29b0bae73452e939b5e9067b947a3da5' % (zip)
 
-#TODO Load JSON data into a Python variable
+response = requests.get(url)
 
+# Load JSON data into a Python variable
+weatherData = json.loads(response.text)
+response.raise_for_status()
+
+# Print weather descriptions.
+# w = weatherData['list']
+city =  weatherData['name']
+print('Current weather in %s:' % (city))
+current = weatherData['weather']
+temp = weatherData['main']['temp']
+tempF = int(1.8*(temp-273) + 32)
+print('Temperature is %s' % (tempF))
+print(current[0]['main'] + " - " + current[0]['description'])
